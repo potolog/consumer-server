3b3d3791be6baa91f5dafbf74e0f207dce7457c0
moneylog <moneylog@naver.com>
2018-07-21 13:29:03 +0900

    소비자불만 repository 추가

---
+++ src/main/java/com/x3800/consumer/repository/ComplainRepository.java
@@ -0,0 +1,7 @@
+package com.x3800.consumer.repository;
+
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+public interface ComplainRepository extends PagingAndSortingRepository {
+
+}
